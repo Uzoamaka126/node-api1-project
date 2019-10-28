9bdfb895177194e7453c88bd03c69d8794d4b740
Uzoamaka126 <vanessapeters126@gmail.com>
2019-10-28 16:09:57 +0100

    Get users both all and individually done

---
+++ app.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const db = require('./data/db');
+
+app.post('/api/users', createNewUser)
+app.get('/api/users/:id', getUserById)
+app.get('/api/users', getAllUsers);
+app.use('*', (req, res) => res.status(404).json({
+    status: 'Not Found',
+    message: 'This route does not exist',
+  }));
+
+function createNewUser(req, res) {
+    const newUser = {
+        name: req.body.name,
+        bio: req.body.bio,
+        created_at: 'now',
+    }
+    db.insert(newUser)
+        .then(res => {
+            console.log(data);
+            db.find(data).then(newlyCreatedUser => {
+                res.status(200).json(data); 
+            })
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                err: "There was an error while saving the user to the db"
+            })
+        })
+}
+function getUserById(req, res) {
+    const { id } = req.params;
+    db.findById(id)
+        .then(data => {
+            // console.log(data)
+            res.status(200).json(data);
+        })
+        .error(err => {
+            console.log(err);
+            res.status(404).json(err);
+        })
+}
+
+function getAllUsers(req, res) {
+    db.find()
+        .then(data => {
+            // console.log(data);
+            res.status(200).json(data);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({err: "There was an error while saving the user to the database"})
+        })
+};
+
+module.exports = app;
--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // implement your API here
+const http = require('http');
+// const hostname = '127.0.0.1'
+const port = process.env.PORT || 3300;
+
+const app = require('./app');
+
+const server = http.createServer(app);
+
+server.listen(port, () => {
+    console.log(`Server has been deployed`)
+})
+
+// Create all the endpoints here
+
